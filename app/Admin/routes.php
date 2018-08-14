38d37e46680bb5bf99b623ad8bb66a2ee6c70001
user <923414405@qq.com>
2018-08-14 19:47:14 +0800

    AA

---
+++ app/Admin/routes.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Routing\Router;
+
+Admin::registerAuthRoutes();
+
+Route::group([
+    'prefix'        => config('admin.route.prefix'),
+    'namespace'     => config('admin.route.namespace'),
+    'middleware'    => config('admin.route.middleware'),
+], function (Router $router) {
+
+    $router->get('/', 'HomeController@index');
+    $router->resource('users', UserController::class); //会员管理
+    $router->resource('article', ArticleController::class); //文章管理
+    $router->resource('article', ArticleController::class); //文章列表
+    $router->resource('category', ArticleCategoryController::class); //文章分类
+    $router->resource('AjaxCategory', AjaxCategory::class); //获取文章分类
+});
